8b7672e91c0b40cd52d5ff27524b2c52647b8813
Sebastien Bourdeauducq <sb@m-labs.hk>
2016-02-10 23:40:02 +0100

    use https for m-labs.hk

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     long_description=open("README").read(),
     author="Sebastien Bourdeauducq",
     author_email="sb@m-labs.hk",
-    url="http://m-labs.hk",
+    url="https://m-labs.hk",
     download_url="https://github.com/m-labs/misoc",
     license="BSD",
     platforms=["Any"],
