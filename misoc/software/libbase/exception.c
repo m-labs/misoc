59a5dede39ef2f3f854da0ce6b00f41d53c1a7d9
whitequark <whitequark@whitequark.org>
2016-02-25 02:03:15 +0000

    libbase: write to UART on unhandled exception.

--- misoc/software/libbase/exception.c
+++ misoc/software/libbase/exception.c
@@ -1,3 +1,6 @@
+#include <generated/csr.h>
+#include <stdio.h>
+
 void isr(void);
 
 #ifdef __or1k__
@@ -12,7 +15,17 @@ void exception_handler(unsigned long vect, unsigned long *regs,
 	if(vect == EXTERNAL_IRQ) {
 		isr();
 	} else {
-		/* Unhandled exception */
+		char outbuf[128];
+		scnprintf(outbuf, sizeof(outbuf),
+		          "\n *** Unhandled exception %d at PC 0x%08x, EA 0x%08x *** \n",
+		          vect, pc, ea);
+
+		char *p = outbuf;
+		while(*p) {
+			while(uart_txfull_read());
+			uart_rxtx_write(*p++);
+		}
+
 		for(;;);
 	}
 }
