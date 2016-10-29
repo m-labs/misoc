4926228dd51d08a814c9cc3e23d8c4379e151d4b
Sebastien Bourdeauducq <sb@m-labs.hk>
2016-10-29 21:27:35 +0800

    kc705: add Ethernet clock constraints

--- misoc/targets/kc705.py
+++ misoc/targets/kc705.py
@@ -121,6 +121,15 @@ class MiniSoC(BaseSoC):
         self.add_wb_slave(mem_decoder(self.mem_map["ethmac"]), self.ethmac.bus)
         self.add_memory_region("ethmac", self.mem_map["ethmac"] | self.shadow_base, 0x2000)
 
+        self.crg.cd_sys.clk.attr.add("keep")
+        self.ethphy.crg.cd_eth_rx.clk.attr.add("keep")
+        self.ethphy.crg.cd_eth_tx.clk.attr.add("keep")
+        self.platform.add_period_constraint(self.ethphy.crg.cd_eth_rx.clk, 8.)
+        self.platform.add_period_constraint(self.ethphy.crg.cd_eth_tx.clk, 8.)
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.ethphy.crg.cd_eth_rx.clk,
+            self.ethphy.crg.cd_eth_tx.clk)
 
 def soc_kc705_args(parser):
     soc_sdram_args(parser)
