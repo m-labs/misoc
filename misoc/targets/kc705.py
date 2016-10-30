ad414a72cab2a5301bf5e3579659ae136831f1be
Sebastien Bourdeauducq <sb@m-labs.hk>
2016-10-30 11:09:14 +0800

    kc705: add period constraints on false path clocks

--- misoc/targets/kc705.py
+++ misoc/targets/kc705.py
@@ -124,6 +124,9 @@ class MiniSoC(BaseSoC):
 
         self.crg.cd_sys.clk.attr.add("keep")
         self.ethphy.crg.cd_eth_tx.clk.attr.add("keep")
+        # period constraints are required here because of vivado
+        self.platform.add_period_constraint(self.crg.cd_sys.clk, 8.0)
+        self.platform.add_period_constraint(self.ethphy.crg.cd_eth_tx.clk, 8.0)
         self.platform.add_false_path_constraints(
             self.crg.cd_sys.clk,
             self.ethphy.crg.cd_eth_tx.clk, eth_clocks.rx)
