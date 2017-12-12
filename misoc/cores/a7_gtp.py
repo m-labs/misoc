2ee3d5496c54d548ac63e3d6391e738ab7169cd6
Sebastien Bourdeauducq <sb@m-labs.hk>
2017-12-12 20:01:44 +0800

    a7gtp: add transceiver initialization code

--- misoc/cores/a7_gtp.py
+++ misoc/cores/a7_gtp.py
@@ -1,7 +1,9 @@
 from collections import namedtuple
+from math import ceil
 
 from migen import *
-
+from migen.genlib.cdc import MultiReg, PulseSynchronizer
+from migen.genlib.fsm import FSM
 
 __all__ = ["QPLLSettings", "QPLLChannel", "QPLL"]
 
@@ -53,3 +55,155 @@ class QPLL(Module):
                 i_RCALENB=1,
                 **channel_settings
             )
+
+
+class GTPTxInit(Module):
+    def __init__(self, sys_clk_freq):
+        self.qpll_reset = Signal()
+        self.qpll_lock = Signal()
+        self.tx_reset = Signal()
+        self.done = Signal()
+
+        # Handle async signals
+        qpll_reset = Signal()
+        tx_reset = Signal()
+        self.sync += [
+            self.qpll_reset.eq(qpll_reset),
+            self.tx_reset.eq(tx_reset)
+        ]
+        self.specials += [
+            NoRetiming(self.qpll_reset),
+            NoRetiming(self.tx_reset)
+        ]
+        qpll_lock = Signal()
+        self.specials += MultiReg(self.qpll_lock, qpll_lock)
+
+        # After configuration, transceiver resets have to stay low for
+        # at least 500ns.
+        # See https://www.xilinx.com/support/answers/43482.html
+        timer_max = ceil(500e-9*sys_clk_freq)
+        timer = Signal(max=timer_max+1)
+        tick = Signal()
+        self.sync += [
+            tick.eq(0),
+            If(timer == timer_max,
+                tick.eq(1),
+                timer.eq(0)
+            ).Else(
+                timer.eq(timer + 1)
+            )
+        ]
+
+        fsm = FSM()
+        self.submodules += fsm
+
+        fsm.act("WAIT",
+            If(tick, NextState("QPLL_RESET"))
+        )
+        fsm.act("QPLL_RESET",
+            tx_reset.eq(1),
+            qpll_reset.eq(1),
+            If(tick, NextState("WAIT_QPLL_LOCK"))
+        )
+        fsm.act("TX_RESET",
+            tx_reset.eq(1),
+            If(qpll_lock & tick, NextState("DONE"))
+        )
+        fsm.act("DONE",
+            self.done.eq(1)
+        )
+
+
+# As usual, Xilinx did not miss the opportunity to mess that up.
+# See: https://www.xilinx.com/support/answers/53561.html
+class GTPRxInit(Module):
+    def __init__(self, sys_clk_freq):
+        self.rx_reset = Signal()
+        self.rx_pma_reset_done = Signal()
+
+        # DRPCLK must be driven by the system clock
+        self.drpaddr = Signal(8)
+        self.drpen = Signal()
+        self.drpdi = Signal(16)
+        self.drprdy = Signal()
+        self.drpdo = Signal(16)
+        self.drpwe = Signal()
+
+        self.enable = Signal()
+        self.restart = Signal()
+        self.done = Signal()
+
+        # Handle async signals()
+        rx_reset = Signal()
+        self.sync += self.rx_reset.eq(rx_reset)
+        self.specials += NoRetiming(self.rx_reset)
+        rx_pma_reset_done = Signal()
+        self.specials += MultiReg(self.rx_pma_reset_done, rx_pma_reset_done)
+
+        drpvalue = Signal(16)
+        drpmask = Signal()
+        self.comb += [
+            self.drpaddr.eq(0x011),
+            If(drpmask,
+                self.drpdi.eq(drpvalue & 0xf7ff)
+            ).Else(
+                self.drpdi.eq(drpvalue)
+            )
+        ]
+
+        rx_pma_reset_done_r = Signal()
+        self.sync += rx_pma_reset_done_r.eq(rx_pma_reset_done)
+
+        fsm = FSM()
+        self.submodules += fsm
+
+        fsm.act("WAIT_ENABLE",
+            If(self.enable, NextState("RESET1"))
+        )
+        fsm.act("GTRXRESET",
+            rx_reset.eq(1),
+            NextState("DRP_READ_ISSUE")
+        )
+        fsm.act("DRP_READ_ISSUE",
+            rx_reset.eq(1),
+            self.drpen.eq(1)
+        )
+        fsm.act("DRP_READ_WAIT",
+            rx_reset.eq(1),
+            If(self.drprdy,
+                NextValue(drpvalue, self.drpdo),
+                NextState("DRP_MOD_ISSUE")
+            )
+        )
+        fsm.act("DRP_MOD_ISSUE",
+            rx_reset.eq(1),
+            drpmask.eq(1),
+            self.drpen.eq(1),
+            self.drpwe.eq(1),
+            NextState("DRP_MOD_WAIT")
+        )
+        fsm.act("DRP_MOD_WAIT",
+            rx_reset.eq(1),
+            If(self.drprdy,
+                NextState("WAIT_PMARST_FALL")
+            )
+        )
+        fsm.act("WAIT_PMARST_FALL",
+            If(rx_pma_reset_done_r & ~rx_pma_reset_done,
+                NextState("DRP_RESTORE_ISSUE")
+            )
+        )
+        fsm.act("DRP_RESTORE_ISSUE",
+            self.drpen.eq(1),
+            self.drpwe.eq(1),
+            NextState("DRP_RESTORE_WAIT")
+        )
+        fsm.act("DRP_RESTORE_WAIT",
+            If(self.drprdy,
+                NextState("DONE")
+            )
+        )
+        fsm.act("DONE",
+            self.done.eq(1),
+            If(self.restart, NextState("WAIT_ENABLE"))
+        )
