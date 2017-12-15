c2a8e91a37a806c5aa77217dea137e5fda15b6b0
Sebastien Bourdeauducq <sb@m-labs.hk>
2017-12-15 21:47:13 +0800

    Revert "bios: longer Ethernet PHY reset pulse"
    This reverts commit 51acb279f59434cb6a3c4b787429e21db4756169.

--- misoc/software/libnet/microudp.c
+++ misoc/software/libnet/microudp.c
@@ -420,10 +420,13 @@ static void busy_wait(unsigned int ds)
 
 void eth_init(void)
 {
-	ethphy_crg_reset_write(1);
-	busy_wait(12500);
 	ethphy_crg_reset_write(0);
-	busy_wait(12500);
+	busy_wait(2);
+	/* that pesky ethernet PHY needs two resets at times... */
+	ethphy_crg_reset_write(1);
+	busy_wait(2);
+	ethphy_crg_reset_write(0);
+	busy_wait(2);
 }
 
 #ifdef CSR_ETHPHY_MODE_DETECTION_MODE_ADDR
