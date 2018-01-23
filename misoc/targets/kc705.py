e01cf4ce2600d4f640427f9a14181c74cad3ba85
Robert Jordens <jordens@gmail.com>
2018-01-23 15:02:32 +0000

    kc705: remove ethernet phy timing constraints
    They are inferred through the plls and we don't need to define clocks
    for false paths anymore.

--- misoc/targets/kc705.py
+++ misoc/targets/kc705.py
@@ -124,11 +124,6 @@ class MiniSoC(BaseSoC):
         self.add_memory_region("ethmac", self.mem_map["ethmac"] | self.shadow_base,
                                ethmac_len)
 
-        self.crg.cd_sys.clk.attr.add("keep")
-        self.ethphy.crg.cd_eth_tx.clk.attr.add("keep")
-        # period constraints are required here because of vivado
-        self.platform.add_period_constraint(self.crg.cd_sys.clk, 8.0)
-        self.platform.add_period_constraint(self.ethphy.crg.cd_eth_tx.clk, 8.0)
         self.platform.add_false_path_constraints(
             self.crg.cd_sys.clk,
             self.ethphy.crg.cd_eth_tx.clk, eth_clocks.rx)
