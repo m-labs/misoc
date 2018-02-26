4dbb89b41295a2c3983e0443b4d884da80f08f91
Sebastien Bourdeauducq <sb@m-labs.hk>
2018-02-27 01:04:33 +0800

    sayma: clean up Ethernet clocking

--- misoc/targets/sayma_amc.py
+++ misoc/targets/sayma_amc.py
@@ -17,16 +17,12 @@ from misoc.integration.builder import *
 
 
 class _CRG(Module):
-    def __init__(self, platform, with_ethernet):
+    def __init__(self, platform):
         self.clock_domains.cd_sys = ClockDomain()
         self.clock_domains.cd_sys4x = ClockDomain(reset_less=True)
         self.clock_domains.cd_clk200 = ClockDomain()
-        if with_ethernet:
-            self.clock_domains.cd_eth_rx = ClockDomain()
-            self.clock_domains.cd_eth_tx = ClockDomain()
 
         clk50 = platform.request("clk50")
-
         clk50_buffered = Signal()
         pll_locked = Signal()
         pll_fb = Signal()
@@ -68,81 +64,14 @@ class _CRG(Module):
         self.specials += Instance("IDELAYCTRL", p_SIM_DEVICE="ULTRASCALE",
             i_REFCLK=ClockSignal("clk200"), i_RST=ic_reset)
 
-        if with_ethernet:
-            eth_clocks = platform.request("eth_clocks")
-
-            # Note: Sharing the main MMCM is not advisable due to the
-            # BUFGCE_DIV phase alignment problem.
-            ethtx_pll_fb = Signal()
-            ethtx_pll_out = Signal()
-            ethtx_pll_out_buffered = Signal()
-            self.specials += [
-                Instance("PLLE2_BASE", name="crg_ethtx_mmcm",
-                    attr={("LOC", "MMCME3_ADV_X1Y4")},
-                    p_STARTUP_WAIT="FALSE",
-
-                    # VCO @ 1GHz
-                    p_REF_JITTER1=0.01, p_CLKIN1_PERIOD=8.0,
-                    p_CLKFBOUT_MULT=8, p_DIVCLK_DIVIDE=1,
-                    i_CLKIN1=self.cd_sys.clk, i_CLKFBIN=ethtx_pll_fb, o_CLKFBOUT=ethtx_pll_fb,
-
-                    # 125MHz
-                    p_CLKOUT0_DIVIDE=8, p_CLKOUT0_PHASE=0.0, o_CLKOUT0=ethtx_pll_out
-                ),
-                Instance("BUFG", i_I=ethtx_pll_out, o_O=ethtx_pll_out_buffered),
-                DDROutput(0, 1, eth_clocks.tx, ethtx_pll_out_buffered)
-            ]
-            self.comb += [
-                self.cd_eth_tx.clk.eq(self.cd_sys.clk),
-                self.cd_eth_tx.rst.eq(self.cd_sys.rst)
-            ]
-
-            rx_clock_buffered = Signal()
-            ethrx_pll_locked = Signal()
-            ethrx_pll_fb = Signal()
-            ethrx_pll_out = Signal()
-            self.specials += [
-                Instance("BUFG", i_I=eth_clocks.rx, o_O=rx_clock_buffered),
-                Instance("PLLE2_BASE", name="crg_ethrx_mmcm",
-                    attr={("LOC", "MMCME3_ADV_X1Y3")},
-                    p_STARTUP_WAIT="FALSE", o_LOCKED=ethrx_pll_locked,
-
-                    # VCO @ 1GHz
-                    p_REF_JITTER1=0.01, p_CLKIN1_PERIOD=8.0,
-                    p_CLKFBOUT_MULT=8, p_DIVCLK_DIVIDE=1,
-                    i_CLKIN1=rx_clock_buffered, i_CLKFBIN=ethrx_pll_fb, o_CLKFBOUT=ethrx_pll_fb,
-
-                    # 125MHz
-                    p_CLKOUT0_DIVIDE=8, p_CLKOUT0_PHASE=0.0, o_CLKOUT0=ethrx_pll_out
-                ),
-                Instance("BUFG", i_I=ethrx_pll_out, o_O=self.cd_eth_rx.clk),
-                AsyncResetSynchronizer(self.cd_eth_rx, ~ethrx_pll_locked),
-            ]
-
-            platform.add_platform_command("set_property CLOCK_DEDICATED_ROUTE BACKBONE [get_nets {sysc}]",
-                sysc=self.cd_sys.clk)
-            platform.add_platform_command("set_property CLOCK_DEDICATED_ROUTE BACKBONE [get_nets {rxc}]",
-                rxc=rx_clock_buffered)
-
-            self.cd_eth_rx.clk.attr.add("keep")
-            platform.add_period_constraint(self.cd_eth_rx.clk, 8.0)
-            platform.add_false_path_constraints(self.cd_sys.clk, self.cd_eth_rx.clk)
-
-            # For mysterious reasons, it doesn't work if you set those values in the
-            # original MMCM instantiation.
-            platform.toolchain.bitstream_commands.extend([
-                "set_property CLKOUT0_PHASE 157.5 [get_cells crg_ethrx_mmcm]",
-                "set_property CLKOUT0_PHASE 180.0 [get_cells crg_ethtx_mmcm]"
-            ])
-
 
 class BaseSoC(SoCSDRAM):
-    def __init__(self, sdram="ddram_64", sdram_controller_type="minicon", clock_ethernet=False, **kwargs):
+    def __init__(self, sdram="ddram_64", sdram_controller_type="minicon", **kwargs):
         platform = sayma_amc.Platform()
         SoCSDRAM.__init__(self, platform, clk_freq=125*1000000,
                           **kwargs)
 
-        self.submodules.crg = _CRG(platform, clock_ethernet)
+        self.submodules.crg = _CRG(platform)
         self.crg.cd_sys.clk.attr.add("keep")
 
         self.submodules.ddrphy = kusddrphy.KUSDDRPHY(platform.request(sdram))
@@ -170,6 +99,70 @@ class BaseSoC(SoCSDRAM):
             self.csr_devices.append("spiflash")
 
 
+class _EthernetCRG(Module):
+    def __init__(self, platform, cd_sys):
+        self.clock_domains.cd_eth_rx = ClockDomain()
+        self.clock_domains.cd_eth_tx = ClockDomain()
+
+        eth_clocks = platform.request("eth_clocks")
+        # Note: Sharing the main MMCM is not advisable due to the
+        # BUFGCE_DIV phase alignment problem.
+        ethtx_pll_fb = Signal()
+        ethtx_pll_out = Signal()
+        ethtx_pll_out_buffered = Signal()
+        self.specials += [
+            Instance("PLLE2_BASE", name="crg_ethtx_mmcm",
+                attr={("LOC", "MMCME3_ADV_X1Y4")},
+                p_STARTUP_WAIT="FALSE",
+
+                # VCO @ 1GHz
+                p_REF_JITTER1=0.01, p_CLKIN1_PERIOD=8.0,
+                p_CLKFBOUT_MULT=8, p_DIVCLK_DIVIDE=1,
+                i_CLKIN1=cd_sys.clk, i_CLKFBIN=ethtx_pll_fb, o_CLKFBOUT=ethtx_pll_fb,
+
+                # 125MHz
+                p_CLKOUT0_DIVIDE=8, p_CLKOUT0_PHASE=180.0, o_CLKOUT0=ethtx_pll_out
+            ),
+            Instance("BUFG", i_I=ethtx_pll_out, o_O=ethtx_pll_out_buffered),
+            DDROutput(0, 1, eth_clocks.tx, ethtx_pll_out_buffered)
+        ]
+        self.comb += [
+            self.cd_eth_tx.clk.eq(cd_sys.clk),
+            self.cd_eth_tx.rst.eq(cd_sys.rst)
+        ]
+
+        rx_clock_buffered = Signal()
+        ethrx_pll_locked = Signal()
+        ethrx_pll_fb = Signal()
+        ethrx_pll_out = Signal()
+        self.specials += [
+            Instance("BUFG", i_I=eth_clocks.rx, o_O=rx_clock_buffered),
+            Instance("PLLE2_BASE", name="crg_ethrx_mmcm",
+                attr={("LOC", "MMCME3_ADV_X1Y3")},
+                p_STARTUP_WAIT="FALSE", o_LOCKED=ethrx_pll_locked,
+
+                # VCO @ 1GHz
+                p_REF_JITTER1=0.01, p_CLKIN1_PERIOD=8.0,
+                p_CLKFBOUT_MULT=8, p_DIVCLK_DIVIDE=1,
+                i_CLKIN1=rx_clock_buffered, i_CLKFBIN=ethrx_pll_fb, o_CLKFBOUT=ethrx_pll_fb,
+
+                # 125MHz
+                p_CLKOUT0_DIVIDE=8, p_CLKOUT0_PHASE=157.5, o_CLKOUT0=ethrx_pll_out
+            ),
+            Instance("BUFG", i_I=ethrx_pll_out, o_O=self.cd_eth_rx.clk),
+            AsyncResetSynchronizer(self.cd_eth_rx, ~ethrx_pll_locked),
+        ]
+
+        platform.add_platform_command("set_property CLOCK_DEDICATED_ROUTE BACKBONE [get_nets {sysc}]",
+            sysc=cd_sys.clk)
+        platform.add_platform_command("set_property CLOCK_DEDICATED_ROUTE BACKBONE [get_nets {rxc}]",
+            rxc=rx_clock_buffered)
+
+        self.cd_eth_rx.clk.attr.add("keep")
+        platform.add_period_constraint(self.cd_eth_rx.clk, 8.0)
+        platform.add_false_path_constraints(cd_sys.clk, self.cd_eth_rx.clk)
+
+
 class MiniSoC(BaseSoC):
     mem_map = {
         "ethmac": 0x30000000,  # (shadow @0xb0000000)
@@ -177,7 +170,9 @@ class MiniSoC(BaseSoC):
     mem_map.update(BaseSoC.mem_map)
 
     def __init__(self, *args, ethmac_nrxslots=2, ethmac_ntxslots=2, **kwargs):
-        BaseSoC.__init__(self, clock_ethernet=True, *args, **kwargs)
+        BaseSoC.__init__(self, *args, **kwargs)
+
+        self.submodules.ethcrg = _EthernetCRG(self.platform, self.crg.cd_sys)
 
         self.csr_devices += ["ethphy", "ethmac"]
         self.interrupt_devices.append("ethmac")
@@ -191,7 +186,6 @@ class MiniSoC(BaseSoC):
         self.add_wb_slave(self.mem_map["ethmac"], ethmac_len, self.ethmac.bus)
         self.add_memory_region("ethmac", self.mem_map["ethmac"] | self.shadow_base,
                                ethmac_len)
-
 
 
 def main():
