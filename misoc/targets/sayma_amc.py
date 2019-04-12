7b75990c03d7ff54c9705e81f34e6365037aa486
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-04-12 19:15:33 +0800

    sayma_amc: add GTH Ethernet timing constraints

--- misoc/targets/sayma_amc.py
+++ misoc/targets/sayma_amc.py
@@ -161,6 +161,15 @@ class MiniSoC(BaseSoC):
         self.add_wb_slave(self.mem_map["ethmac"], ethmac_len, self.ethmac.bus)
         self.add_memory_region("ethmac", self.mem_map["ethmac"] | self.shadow_base,
                                ethmac_len)
+        self.crg.cd_sys.clk.attr.add("keep")
+        self.ethphy.cd_eth_rx.clk.attr.add("keep")
+        self.ethphy.cd_eth_tx.clk.attr.add("keep")
+        self.platform.add_period_constraint(self.ethphy.cd_eth_rx.clk, 1e9/125e6)
+        self.platform.add_period_constraint(self.ethphy.cd_eth_tx.clk, 1e9/125e6)
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.ethphy.cd_eth_rx.clk,
+            self.ethphy.cd_eth_tx.clk)
 
 
 def soc_sayma_amc_args(parser):
