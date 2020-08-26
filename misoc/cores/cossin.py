06352ddc13dcd9e4416f81b00a6f6da78157d14b
Robert Jördens <rj@quartiq.de>
2020-08-26 14:34:44 +0000

    duc, cossin: add DSP tools from phaser

---
+++ misoc/cores/cossin.py
@@ -0,0 +1,199 @@
+import migen as mg
+import numpy as np
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class CosSinGen(mg.Module):
+    """cos(z), sin(z) generator using a block ROM and linear interpolation
+
+    For background information about an alternative way of computing
+    trigonometric functions without multipliers and large ROM, see:
+
+    P. K. Meher et al., "50 Years of CORDIC: Algorithms, Architectures,
+    and Applications"
+    in IEEE Transactions on Circuits and Systems I: Regular Papers, vol. 56, no. 9,
+    pp. 1893-1907, Sept. 2009. doi: 10.1109/TCSI.2009.2025803
+    https://eprints.soton.ac.uk/267873/1/tcas1_cordic_review.pdf
+
+    For other implementations of trigonometric function generators, see
+
+    https://www.xilinx.com/products/intellectual-property/dds_compiler.html#documentation
+    https://www.intel.com/content/dam/altera-www/global/en_US/pdfs/literature/ug/ug_nco.pdf
+
+    The implementation is as follows:
+
+    1. Extract the 3 MSBs and save for later unmapping.
+    2. Map the remaining LSBs into the first octant [0, pi/4[
+       (conditional phase flip)
+    3. Use the coarse `zl` MSBs of the first octant phase to look up
+       cos(z), sin(z), cos'(z), sin'(z) in block ROM.
+    4. Interpolate with the residual LSBs as cos(z + dz) = cos(z) + dz*cos'(z).
+    5. Unmap the octant (cos sign flip, sin sign flip, cos/sin swap).
+
+    The default values for the constructor parameters yield a 100 dBc
+    SFDR cons/sin generator with 18 bit phase and 16 bit outputs using
+    one 9x36 bit block ROM (one RAMB18xx in read-only SDP mode on several
+    Xilinx architectures), and 4x6 and 3x6 bit fabric multipliers for the
+    interpolation. It runs at > 250 MHz on an A7-2.
+
+    Dithering the input phase improves the SFDR further.
+
+    The output is combinatorial and it helps to add another pipeline
+    stage.
+
+    Multiplication by a amplitude scaling factor (`a*cos(z)`)
+    and generation of the phase input (e.g. a phase accumulator)
+    is to be implemented elsewhere.
+
+    Using a second port of an existing LUT is supported by passing the
+    existing `Memory` as `use_lut`.
+    """
+    def __init__(self, z=18, x=15, zl=9, xd=4, backoff=None, use_lut=None):
+        self.latency = 0  # computed later
+        self.z = mg.Signal(z)  # input phase
+        self.x = mg.Signal((x + 1, True), reset_less=True)  # output cos(z)
+        self.y = mg.Signal((x + 1, True), reset_less=True)  # output sin(z)
+
+        ###
+
+        if backoff is None:
+            backoff = min(3, (1 << x - 1) - 1)
+        self.x_max = (1 << x) - backoff
+
+        # LUT depth
+        if zl is None:
+            zl = z - 3
+        assert zl >= 0
+
+        # generate the cos/sin LUT
+        a = np.exp(1j*np.pi/4/(1 << zl)*(np.arange(1 << zl) + .5))
+        cs = np.round(self.x_max*a)
+        csd = np.round(np.pi/4/(1 << x - xd)*cs)
+
+        lut_init = []
+        for csi, csdi in zip(cs, csd):
+            # save a bit by noticing that cos(z) > 1/2 for 0 < z < pi/4
+            xy = csi - (1 << x - 1)
+            xi, yi = int(xy.real), int(xy.imag)
+            assert 0 <= xi < 1 << x - 1, csi
+            assert 0 <= yi < 1 << x, csi
+            lut_init.append(xi | (yi << x - 1))
+            if xd:
+                # derivative LUT
+                # save a bit again
+                xyd = csdi - (1 << xd - 1)
+                xid, yid = int(xyd.real), int(xyd.imag)
+                assert 0 <= xid < 1 << xd - 1, csdi
+                assert 0 <= yid < 1 << xd, csdi
+                lut_init[-1] |= (xid << 2*x - 1) | (yid << 2*x + xd - 2)
+
+        # LUT ROM
+        mem_layout = [("x", x - 1), ("y", x)]
+        if xd:
+            mem_layout.extend([("xd", xd - 1), ("yd", xd)])
+        lut_data = mg.Record(mem_layout, reset_less=True)
+        assert len(lut_init) == 1 << zl
+        assert all(0 <= _ < 1 << len(lut_data) for _ in lut_init)
+        logger.info("CosSin LUT {} bit deep, {} bit wide".format(
+            zl, len(lut_data)))
+        if use_lut is not None:
+            assert all(a == b for a, b in zip(use_lut.init, lut_init))
+            self.lut = use_lut
+        else:
+            self.lut = mg.Memory(len(lut_data), 1 << zl, init=lut_init)
+            self.specials += self.lut
+        lut_port = self.lut.get_port()
+        self.specials += lut_port
+
+        self.sync += [
+            # use BRAM output data register
+            lut_data.raw_bits().eq(lut_port.dat_r),
+        ]
+        self.latency += 1  # mem dat_r output register
+
+        # compute LUT address
+        # 3 MSBs: octant
+        # LSBs: phase, maped into first octant
+        za = mg.Signal(z - 3)
+        self.comb += [
+            za.eq(mg.Mux(
+                self.z[-3], (1 << z - 3) - 1 - self.z[:-3], self.z[:-3])),
+            lut_port.adr.eq(za[-zl:]),
+        ]
+        self.latency += 1  # mem address register
+
+        if xd:  # apply linear interpolation
+            zk = z - 3 - zl
+            zd = mg.Signal((zk + 1, True), reset_less=True)
+            self.comb += zd.eq(za[:zk] - (1 << zk - 1) + self.z[-3])
+            zd = self.pipe(zd, self.latency)
+            # add a rounding bias
+            zq = z - 3 - x + xd
+            assert zq > 0
+            qb = (1 << zq - 1) - 1
+            lxd = mg.Signal((xd + zk, True), reset_less=True)
+            lyd = mg.Signal((xd + zk, True), reset_less=True)
+            self.sync += [
+                lxd.eq(zd*(lut_data.xd | (1 << xd - 1))),
+                lyd.eq(zd*lut_data.yd),
+            ]
+            x1 = self.pipe(self.pipe(lut_data.x | (1 << x - 1), 1) - ((lyd + qb) >> zq), 1)
+            y1 = self.pipe(self.pipe(lut_data.y, 1) + ((lxd + qb) >> zq), 1)
+            self.latency += 2
+        else:
+            x1 = self.pipe(lut_data.x | (1 << x - 1), 0)
+            y1 = self.pipe(lut_data.y, 0)
+
+        # unmap octant
+        zq = self.pipe(mg.Cat(self.z[-3] ^ self.z[-2],
+                              self.z[-2] ^ self.z[-1], self.z[-1]), self.latency)
+        # intermediate unmapping signals
+        x2 = self.pipe(mg.Mux(zq[0], y1, x1), 0)
+        y2 = self.pipe(mg.Mux(zq[0], x1, y1), 0)
+        self.comb += [
+            self.x.eq(mg.Mux(zq[1], -x2, x2)),
+            self.y.eq(mg.Mux(zq[2], -y2, y2)),
+        ]
+
+    def pipe(self, x, n=0):
+        """Create `n` pipeline register stages for signal x
+        and return final stage"""
+        k = mg.value_bits_sign(x)
+        x, x0 = mg.Signal(k, reset_less=True), x
+        self.comb += x.eq(x0)
+        for i in range(n):
+            x, x0 = mg.Signal(k, reset_less=True), x
+            self.sync += x.eq(x0)
+        return x
+
+    def log(self, z, xy):
+        """Run self for each value of `z` and record output values into `xy`"""
+        if z is None:
+            z = np.arange(1 << len(self.z))
+        z = np.r_[z, (0,)*self.latency]
+        for i, zi in enumerate(z):
+            yield self.z.eq(int(zi))
+            yield
+            if i >= self.latency:
+                x = yield self.x
+                y = yield self.y
+                xy.append((x, y))
+
+    def xy_err(self, xy):
+        """Given the `xy` output of all possible `z` values,
+        calculate error, maximum quadrature error, rms magnitude error,
+        and maximum magnitude error."""
+        z = np.arange(1 << len(self.z))
+        x, y = np.array(xy).T
+        xy = x + 1j*y
+        pxy = np.fft.fft(xy)
+        assert np.argmax(np.absolute(pxy)) == 1
+        pxy[1] = 0.
+        xye = np.fft.ifft(pxy)
+        xye2 = np.absolute(xye)
+        assert xye.mean() < 1e-3
+        return (xye, np.fabs(np.r_[xye.real, xye.imag]).max(),
+                (xye2**2).mean()**.5, xye2.max())
---
+++ misoc/cores/duc.py
@@ -0,0 +1,315 @@
+from migen import *
+from misoc.interconnect.stream import Endpoint
+from migen.genlib.fsm import FSM
+
+from .cossin import CosSinGen
+
+
+def complex(width):
+    """Complex integer layout"""
+    return [("i", (width, True)), ("q", (width, True))]
+
+
+def eqh(lhs, rhs):
+    """MSB aligned assignment.
+    Returns statement to be added to comb/sync context."""
+    shift = len(lhs) - len(rhs)
+    if shift > 0:
+        return lhs[shift:].eq(rhs)
+    elif shift < 0:
+        return lhs.eq(rhs[-shift:])
+    else:
+        return lhs.eq(rhs)
+
+
+def pipe(lhs, rhs, n):
+    """Assign rhs to lhs through n reset_less pipeline registers.
+    Returns list of statements to be added to sync context."""
+    assert n > 0
+    stmts = []
+    w = min(len(lhs), len(rhs))
+    for i in range(n - 1):
+        pip, rhs = rhs, Signal(n, reset_less=True)
+        stmts.append(rhs.eq(pip))
+    stmts.append(lhs.eq(rhs))
+    return stmts
+
+
+class ComplexMultiplier(Module):
+    def __init__(self, awidth=16, bwidth=None, pwidth=None):
+        """
+        Complex multiplier, with full pipelining, using 3 DSP, rounding
+
+        `p.i + 1j*p.q = (a.i + 1j*a.q)*(b.i + 1j*b.q)`
+
+        Output scaling and rounding for `pwidth < awidth + bwidth + 1`:
+        * Rounding is "round half down".
+        * If `|a| <= (1 << awidth - 1) - 1`, or
+            `|b| <= (1 << bwidth - 1) - 1`, then
+            `p.i`, `p.q`, |p| will be valid.
+        * Ensure that |a| and |b| are in range and not just their
+          quadratures.
+        * That range excludes the components' (negative) minimum of at least
+          one input, that input's unit circle, and the area outside the unit
+          circles of both inputs.
+        """
+        if bwidth is None:
+            bwidth = awidth
+        if pwidth is None:
+            # worst case min*min+min*min
+            pwidth = awidth + bwidth + 1
+        self.a = Record(complex(awidth), reset_less=True)  # 5
+        self.b = Record(complex(bwidth), reset_less=True)  # 5
+        self.p = Record(complex(pwidth), reset_less=True)
+
+        # with rounding the worst case is assumed (!) to be max*max
+        # (unit circle interior: 2 bit smaller than full width worst
+        # case above)
+        bias_bits = max(0, (awidth + bwidth - 1) - pwidth)
+        # rounding bias constant
+        # we don't implement more complicated rounding (even/odd) because
+        # doing so looks like it might not fit into the DSPs and
+        # due to the typically large shift the remaining bias is small.
+        bias = (1 << bias_bits - 1) - 1 if bias_bits > 0 else 0
+
+        ai = [Signal((awidth, True), reset_less=True) for _ in range(3)]
+        aq = [Signal((awidth, True), reset_less=True) for _ in range(3)]
+        bi = [Signal((bwidth, True), reset_less=True) for _ in range(2)]
+        bq = [Signal((bwidth, True), reset_less=True) for _ in range(2)]
+        ad = Signal((awidth + 1, True), reset_less=True)
+        bs = Signal((bwidth + 1, True), reset_less=True)
+        bd = Signal((bwidth + 1, True), reset_less=True)
+        # these needs yet another (temporary) bit since the synthesizer
+        # usually doesn't prove the cancellation
+        m = [Signal((awidth + bwidth + 2, True), reset_less=True)
+             for _ in range(8)]
+        self.sync += [
+            Cat(ai).eq(Cat(self.a.i, ai)),  # 1-3
+            Cat(aq).eq(Cat(self.a.q, aq)),  # 1-3
+            Cat(bi).eq(Cat(self.b.i, bi)),  # 1-2
+            Cat(bq).eq(Cat(self.b.q, bq)),  # 1-2
+            ad.eq(self.a.i + self.a.q),  # 1
+            m[0].eq(ad*bi[0]),  # 2
+            m[1].eq(m[0] + bias),  # 3
+            bs.eq(bi[1] + bq[1]),  # 3
+            bd.eq(bi[1] - bq[1]),  # 3
+            m[2].eq(bs*aq[2]),  # 4
+            m[3].eq(bd*ai[2]),  # 4
+            m[4].eq(m[1]),  # 4
+            m[5].eq(m[1]),  # 4
+            m[6].eq(m[4] - m[2]),  # 5
+            m[7].eq(m[5] - m[3]),  # 5
+        ]
+        self.comb += [
+            self.p.i.eq(m[6][bias_bits:]),
+            self.p.q.eq(m[7][bias_bits:]),
+        ]
+        self.latency = 5
+
+
+class Accu(Module):
+    """Phase accumulator, with frequency, phase offset and clear"""
+    def __init__(self, fwidth, pwidth):
+        self.f = Signal(fwidth)  # 2
+        self.p = Signal(pwidth)  # 1
+        self.clr = Signal(reset=1)  # 2
+        self.z = Signal(pwidth, reset_less=True)
+        # reset by clr
+        q = Signal(fwidth, reset_less=True)
+        self.sync += [
+            q.eq(q + self.f),
+            If(self.clr,
+                q.eq(0),
+            ),
+            self.z.eq(self.p + q[-pwidth:]),
+        ]
+
+
+class MCM(Module):
+    """Multiple constant multiplication
+
+    Multiplies the input by multiple small constants.
+    """
+    def __init__(self, width, constants):
+        n = len(constants)
+        self.i = i = Signal(width, reset_less=True)  # 1
+        self.o = o = [Signal.like(self.i) for i in range(n)]
+
+        ###
+
+        # TODO: improve MCM
+        assert n <= 9
+        assert range(n) == constants
+
+        ctx = self.comb
+        if n > 0:
+            ctx += o[0].eq(0)
+        if n > 1:
+            ctx += o[1].eq(i)
+        if n > 2:
+            ctx += o[2].eq(i << 1)
+        if n > 3:
+            ctx += o[3].eq(i + (i << 1))
+        if n > 4:
+            ctx += o[4].eq(i << 2)
+        if n > 5:
+            ctx += o[5].eq(i + (i << 2))
+        if n > 6:
+            ctx += o[6].eq(o[3] << 1)
+        if n > 7:
+            ctx += o[7].eq((i << 3) - i)
+        if n > 8:
+            ctx += o[8].eq(i << 3)
+
+
+class PhasedAccu(Module):
+    """Phase accumulator with multiple phased outputs.
+
+    Output data (across cycles and outputs) is such
+    that there is always one frequency word offset between successive
+    phase samples.
+
+    * Input frequency, phase offset, clear
+    * Output `n` phase samples per cycle
+    """
+    def __init__(self, n, fwidth, pwidth):
+        self.f = Signal(fwidth)
+        self.p = Signal(pwidth)
+        self.clr = Signal(reset=1)
+        self.z = [Signal(pwidth, reset_less=True)
+                  for _ in range(n)]
+
+        self.submodules.mcm = MCM(fwidth, range(n))
+        # reset by clr
+        qa = Signal(fwidth, reset_less=True)
+        qb = Signal(fwidth, reset_less=True)
+        clr_d = Signal(reset_less=True)
+        self.sync += [
+            clr_d.eq(self.clr),
+            qa.eq(qa + (self.f << log2_int(n))),
+            self.mcm.i.eq(self.f),
+            If(self.clr | clr_d,
+                qa.eq(0),
+            ),
+            If(clr_d,
+                self.mcm.i.eq(0),
+            ),
+            qb.eq(qa + (self.p << fwidth - pwidth)),
+            [z.eq((qb + oi)[fwidth - pwidth:])
+                for oi, z in zip(self.mcm.o, self.z)]
+        ]
+
+
+class PhaseModulator(Module):
+    """Complex phase modulator/shifter.
+
+    * Shifts input `i` by phase `z`
+    * Output `o`
+    """
+    def __init__(self, **kwargs):
+        self.submodules.cs = CosSinGen(**kwargs)
+        self.submodules.mul = ComplexMultiplier(
+            awidth=len(self.cs.x), pwidth=len(self.cs.x))
+        self.z = self.cs.z  # cs.z + 1 + mul.a
+        self.i = self.mul.b  # mul.b
+        self.o = self.mul.p
+        self.sync += [
+            self.mul.a.i.eq(self.cs.x),
+            self.mul.a.q.eq(self.cs.y),
+        ]
+        self.latency = self.cs.latency + 1 + self.mul.latency
+
+
+class MultiDDS(Accu):
+    """Time division multiplexed oscillator.
+
+    Uses one CosSinGen and one (complex-real) multiplier.
+    DDS parameter latencies are unmatched between parameters
+    and channels. Overflowing summation.
+    """
+    def __init__(self, n, fwidth, xwidth):
+        self.i = [Record([
+            ("f", fwidth), ("p", xwidth), ("a", xwidth - 1), ("clr", 1)])
+                  for i in range(n)]
+        self.o = Record(complex(xwidth), reset_less=True)
+        self.stb = Signal()
+        self.valid = Signal()
+
+        self.submodules.mod = PhaseModulator()
+        # accu
+        accu = [Signal(fwidth) for _ in range(n)]
+        run = Signal()
+
+        i = Signal(max=n)
+        self._i = i
+        def cycle(offset):
+            return i == offset % n
+
+        for ii, ctrl in enumerate(self.i):
+            self.sync += [
+                If(run & cycle(ii),
+                    accu[ii].eq(accu[ii] + ctrl.f),
+                    If(ctrl.clr,
+                        accu[ii].eq(0)
+                    ),
+                ),
+                If(cycle(ii + 1),
+                    self.mod.z.eq(
+                        (accu[ii] + (ctrl.p << fwidth - len(ctrl.p))
+                         )[fwidth - len(self.mod.z):]),
+                ),
+                If(cycle(ii + 2 + self.mod.cs.latency),
+                    self.mod.i.i.eq(ctrl.a),
+                ),
+            ]
+
+        # 2: q, z
+        latency = self.mod.latency + 2
+
+        self.sync += [
+            If(run,
+                i.eq(i + 1),
+            ),
+            If(cycle(n - 1),
+                i.eq(0),
+                run.eq(0),
+            ),
+            If(self.stb,
+                run.eq(1),
+            ),
+            If(run,
+                self.o.i.eq(Mux(cycle(latency), 0, self.o.i) +
+                            self.mod.o.i),
+                self.o.q.eq(Mux(cycle(latency), 0, self.o.q) +
+                            self.mod.o.q),
+            ),
+            self.valid.eq(run & cycle(latency - 1)),
+        ]
+
+
+
+class PhasedDUC(Module):
+    """Phased (multi-sample) digital upconverter/frequency shifter.
+
+    * Input phased complex input sample index `j` as `i[j]`
+    * Shift by frequency `f`, phase `p` (phase accumulator clear as `clr`).
+    * Output phased sample index `j` as `o[j]`.
+    """
+    def __init__(self, zl=10, **kwargs):
+        self.submodules.accu = PhasedAccu(**kwargs)
+        self.f, self.p, self.clr = self.accu.f, self.accu.p, self.accu.clr
+        self.i = []
+        self.o = []
+        self.mods = []
+        for i in range(len(self.accu.z)):
+            if i & 1:
+                use_lut = self.mods[i - 1].cs.lut
+            else:
+                use_lut = None
+            mod = PhaseModulator(z=len(self.accu.z[0]), zl=zl,
+                    x=15, use_lut=use_lut)
+            self.mods.append(mod)
+            self.comb += mod.z.eq(self.accu.z[i])
+            self.i.append(mod.i)
+            self.o.append(mod.o)
+        self.submodules += self.mods
