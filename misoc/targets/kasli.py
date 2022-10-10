4fb0730db4c5de7e86f82fa3bd204e6c4608af85
Spaqin <mw@m-labs.hk>
2022-10-10 17:55:41 +0800

    kasli: SYS/RTIO clock merge (#129)

--- misoc/targets/kasli.py
+++ misoc/targets/kasli.py
@@ -4,6 +4,7 @@ import argparse
 
 from migen import *
 from migen.genlib.resetsync import AsyncResetSynchronizer
+from migen.genlib.cdc import MultiReg
 from migen.build.platforms.sinara import kasli
 
 from misoc.cores.sdram_settings import MT41K256M16
@@ -14,6 +15,7 @@ from misoc.cores.liteeth_mini.phy.a7_1000basex import A7_1000BASEX
 from misoc.cores.liteeth_mini.mac import LiteEthMAC
 from misoc.integration.soc_sdram import *
 from misoc.integration.builder import *
+from misoc.interconnect.csr import *
 
 
 class AsyncResetSynchronizerBUFG(Module):
@@ -34,7 +36,186 @@ class AsyncResetSynchronizerBUFG(Module):
         ]
 
 
-class _CRG(Module):
+class ClockSwitchFSM(Module):
+    def __init__(self):
+        self.i_clk_sw = Signal()
+
+        self.o_clk_sw = Signal()
+        self.o_reset = Signal()
+
+        ###
+
+        i_switch = Signal()
+        o_switch = Signal()
+        reset = Signal()
+
+        # at 62.5MHz bootstrap cd, will get around 1ms
+        delay_counter = Signal(16, reset=0xFFFF)
+
+        # register to prevent glitches
+        self.sync.bootstrap += [
+            self.o_clk_sw.eq(o_switch),
+            self.o_reset.eq(reset),
+        ]
+
+        self.o_clk_sw.attr.add("no_retiming")
+        self.o_reset.attr.add("no_retiming")
+        self.i_clk_sw.attr.add("no_retiming")
+        i_switch.attr.add("no_retiming")
+
+        self.specials += MultiReg(self.i_clk_sw, i_switch, "bootstrap")
+
+        fsm = ClockDomainsRenamer("bootstrap")(FSM(reset_state="START"))
+
+        self.submodules += fsm
+
+        fsm.act("START",
+            If(i_switch & ~o_switch,
+                NextState("RESET_START"))
+        )
+        
+        fsm.act("RESET_START",
+            reset.eq(1),
+            If(delay_counter == 0,
+                NextValue(delay_counter, 0xFFFF),
+                NextState("CLOCK_SWITCH")
+            ).Else(
+                NextValue(delay_counter, delay_counter-1),
+            )
+        )
+
+        fsm.act("CLOCK_SWITCH",
+            reset.eq(1),
+            NextValue(o_switch, 1),
+            NextValue(delay_counter, delay_counter-1),
+            If(delay_counter == 0,
+                NextState("START"))
+        )
+
+
+class _RtioSysCRG(Module, AutoCSR):
+    def __init__(self, platform):
+        self.clock_domains.cd_sys = ClockDomain()
+        self.clock_domains.cd_sys4x = ClockDomain(reset_less=True)
+        self.clock_domains.cd_sys4x_dqs = ClockDomain(reset_less=True)
+        self.clock_domains.cd_clk200 = ClockDomain()
+
+        # for FSM only
+        self.clock_domains.cd_bootstrap = ClockDomain(reset_less=True)
+
+        self.clock_sel = CSRStorage()
+        self.switch_done = CSRStatus()
+
+        # bootstrap clock
+        clk125 = platform.request("clk125_gtp")
+        platform.add_period_constraint(clk125, 8.)
+        self.clk125_buf = Signal()
+        self.clk125_div2 = Signal()
+        self.specials += Instance("IBUFDS_GTE2",
+            i_CEB=0,
+            i_I=clk125.p, i_IB=clk125.n,
+            o_O=self.clk125_buf,
+            o_ODIV2=self.clk125_div2)
+        
+        self.specials += Instance("BUFG", i_I=self.clk125_div2, o_O=self.cd_bootstrap.clk)
+
+        # "rtio" clock (mgt flavor)
+        if platform.hw_rev == "v2.0":
+            cdr_clk_out = platform.request("cdr_clk_clean")
+        else:
+            cdr_clk_out = platform.request("si5324_clkout")
+        
+        self.cdr_clk_buf = Signal()
+        cdr_clk_div2 = Signal()
+
+        platform.add_period_constraint(cdr_clk_out, 8.)
+
+        self.specials += Instance("IBUFDS_GTE2",
+            i_CEB=0,
+            i_I=cdr_clk_out.p, i_IB=cdr_clk_out.n,
+            o_O=self.cdr_clk_buf,
+            o_ODIV2=cdr_clk_div2)
+
+        clk_sw_fsm = ClockSwitchFSM()
+        self.submodules += clk_sw_fsm
+
+        mmcm_locked = Signal()
+        mmcm_fb_in = Signal()
+        mmcm_fb_out = Signal()
+        mmcm_sys = Signal()
+        mmcm_sys4x = Signal()
+        mmcm_sys4x_dqs = Signal()
+
+        pll_clk200 = Signal()
+        pll_fb = Signal()
+        pll_locked = Signal()
+        self.specials += [
+            Instance("MMCME2_ADV",
+                p_CLKIN1_PERIOD=16.0,
+                i_CLKIN1=cdr_clk_div2,
+                i_CLKIN2=self.clk125_div2,
+
+                i_CLKINSEL=clk_sw_fsm.o_clk_sw,
+                i_RST=clk_sw_fsm.o_reset,
+
+                i_CLKFBIN=mmcm_fb_in,
+                o_CLKFBOUT=mmcm_fb_out,
+                o_LOCKED=mmcm_locked,
+
+                # VCO @ 1GHz with MULT=16
+                p_CLKFBOUT_MULT_F=16, p_DIVCLK_DIVIDE=1,
+
+                # ~125MHz
+                # kasli 1.1 seems to still be within timing on that
+                p_CLKOUT0_DIVIDE_F=8, p_CLKOUT0_PHASE=0.0, o_CLKOUT0=mmcm_sys,
+
+                # ~500MHz. Must be more than 400MHz as per DDR3 specs.
+                p_CLKOUT1_DIVIDE=2, p_CLKOUT1_PHASE=0.0, o_CLKOUT1=mmcm_sys4x,
+                p_CLKOUT2_DIVIDE=2, p_CLKOUT2_PHASE=90.0, o_CLKOUT2=mmcm_sys4x_dqs,
+            ),
+            Instance("PLLE2_BASE",
+                p_CLKIN1_PERIOD=16.0,
+                i_CLKIN1=self.clk125_div2,
+
+                i_CLKFBIN=pll_fb,
+                o_CLKFBOUT=pll_fb,
+                o_LOCKED=pll_locked,
+
+                # VCO @ 1GHz
+                p_CLKFBOUT_MULT=16, p_DIVCLK_DIVIDE=1,
+
+                # 200MHz for IDELAYCTRL
+                p_CLKOUT0_DIVIDE=5, p_CLKOUT0_PHASE=0.0, o_CLKOUT0=pll_clk200,
+            ),
+            Instance("BUFG", i_I=mmcm_sys, o_O=self.cd_sys.clk),
+            Instance("BUFG", i_I=mmcm_sys4x, o_O=self.cd_sys4x.clk),
+            Instance("BUFG", i_I=mmcm_sys4x_dqs, o_O=self.cd_sys4x_dqs.clk),
+            Instance("BUFG", i_I=pll_clk200, o_O=self.cd_clk200.clk),
+            Instance("BUFG", i_I=mmcm_fb_out, o_O=mmcm_fb_in),
+            AsyncResetSynchronizer(self.cd_clk200, ~pll_locked),
+        ]
+
+        # reset if MMCM loses lock or when switching
+        self.submodules += AsyncResetSynchronizerBUFG(self.cd_sys, ~mmcm_locked | clk_sw_fsm.o_reset)
+
+        self.comb += clk_sw_fsm.i_clk_sw.eq(self.clock_sel.storage)
+        self.sync += self.switch_done.status.eq(clk_sw_fsm.o_clk_sw)
+
+        platform.add_false_path_constraints(self.clk125_buf,
+            self.cd_sys.clk)
+
+        reset_counter = Signal(4, reset=15)
+        ic_reset = Signal(reset=1)
+        self.sync.clk200 += \
+            If(reset_counter != 0,
+                reset_counter.eq(reset_counter - 1)
+            ).Else(
+                ic_reset.eq(0)
+            )
+        self.specials += Instance("IDELAYCTRL", i_REFCLK=ClockSignal("clk200"), i_RST=ic_reset)
+
+
+class _SysCRG(Module):
     def __init__(self, platform):
         self.clock_domains.cd_sys = ClockDomain()
         self.clock_domains.cd_sys4x = ClockDomain(reset_less=True)
@@ -112,19 +293,25 @@ class _CRG(Module):
 
 
 class BaseSoC(SoCSDRAM):
-    def __init__(self, sdram_controller_type="minicon", hw_rev=None,
-                 **kwargs):
+    def __init__(self, sdram_controller_type="minicon", hw_rev=None, rtio_sys_merge=False, 
+                 clk_freq=None, **kwargs):
         if hw_rev is None:
             hw_rev = "v1.0"
         platform = kasli.Platform(hw_rev=hw_rev)
 
-        SoCSDRAM.__init__(self, platform,
-                          clk_freq=125e6*14.5/16, cpu_reset_address=0x400000,
-                          **kwargs)
+        if not clk_freq:
+            clk_freq = 125e6 if rtio_sys_merge else 125e6*14.5/16
+
+        SoCSDRAM.__init__(self, platform, cpu_reset_address=0x400000, clk_freq=clk_freq, **kwargs)
+
+        if rtio_sys_merge:
+            self.submodules.crg = _RtioSysCRG(platform)
+            self.csr_devices.append("crg")
+        else:
+            self.submodules.crg = _SysCRG(platform)
 
         self.config["HW_REV"] = hw_rev
 
-        self.submodules.crg = _CRG(platform)
         self.platform.add_period_constraint(self.crg.cd_sys.clk, 1e9/self.clk_freq)
 
         self.submodules.ddrphy = a7ddrphy.A7DDRPHY(platform.request("ddram"))
